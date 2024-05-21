0a82c4f9ddadacd6333a5f9d2592a43094c9bd68
Reactmore <reactmorecom@gmail.com>
2024-05-21 23:35:31 +0700

    support database user and password

--- config.js
+++ config.js
@@ -1,21 +1,59 @@
 require("dotenv").config();
-
-DATABASE_URL = process.env.DATABASE_URL === undefined ? './bot.db' : process.env.DATABASE_URL;
 const { Sequelize } = require('sequelize');
 
-module.exports ={
-    DATABASE_URL: DATABASE_URL,
-    DATABASE: DATABASE_URL === './bot.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: false }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: false }), 
-    apiId : Number(process.env.API_ID),
-    apiHash : process.env.API_HASH,
-    session : process.env.SESSION ? process.env.SESSION : "",
-    BOT_TOKEN:process.env.BOT_TOKEN,
-    sudo:this.sudo,
-    DEVELOPMENT:process.env.STATE === undefined ? false : process.env.STATE,    
-    setSudo:function(s) {
+const DATABASE_URL = process.env.DATABASE_URL === undefined ? './bot.db' : process.env.DATABASE_URL;
+
+
+let DATABASE;
+
+if (DATABASE_URL === './bot.db') {
+    DATABASE = new Sequelize({
+        dialect: "sqlite",
+        storage: DATABASE_URL,
+        logging: false,
+    });
+} else if (DATABASE_URL === "mysql") {
+    DATABASE = new Sequelize(
+        process.env.DATABASE_NAME,
+        process.env.DATABASE_USERNAME,
+        process.env.DATABASE_PASSWORD,
+        {
+            host: process.env.DATABASE_HOST || '127.0.0.1',
+            dialect: DATABASE_URL,
+            logging: process.env.STATE === undefined ? FALSE : console.log,
+            define: {
+                freezeTableName: true,
+            },
+        }, 
+
+    );
+} else {
+    new Sequelize(DATABASE_URL, {
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
+        logging: false,
+    });
+}
+
+
+
+module.exports = {
+    DATABASE_URL,
+    DATABASE,
+    apiId: Number(process.env.API_ID),
+    apiHash: process.env.API_HASH,
+    session: process.env.SESSION ? process.env.SESSION : "",
+    BOT_TOKEN: process.env.BOT_TOKEN,
+    sudo: this.sudo,
+    DEVELOPMENT: process.env.STATE === undefined ? false : process.env.STATE,
+    setSudo: function (s) {
         this.sudo = s;
     },
-    getSudo:function(){
+    getSudo: function () {
         return this.sudo;
     }
 }
