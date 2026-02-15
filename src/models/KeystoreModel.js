60d338b08423354cf65d1a50e4051d392d89ba0b
reactmore <reactmorecom@gmail.com>
2026-02-16 03:03:37 +0700

    revert keystore model for helper

---
+++ src/models/KeystoreModel.js
@@ -0,0 +1,33 @@
+const { DataTypes, Op } = require("sequelize");
+const BaseModel = require("./BaseModel");
+
+class KeystoreModel extends BaseModel {
+    static init(sequelize) {
+        return super.initModel(
+            {
+                key: {
+                    type: DataTypes.STRING,
+                    allowNull: false,
+                    unique: true,
+                },
+                value: {
+                    type: DataTypes.TEXT,
+                    allowNull: false,
+                },
+            },
+            {
+                tableName: 'keystore',
+                modelName: "KeystoreModel",
+                timestamps: true,
+                freezeTableName: true,
+            },
+            sequelize
+        );
+    }
+
+    static associate(models) {
+
+    }
+}
+
+module.exports = KeystoreModel;
